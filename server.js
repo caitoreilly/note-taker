1079673ca927fe0502f9f03dd024cc5e008cde51
Caitlin O'Reilly <coreilly29@gmail.com>
2022-10-16 13:58:25 -0400

    npm initialized, express installed, path and util dependencies added, util promises added for readfile and writefile, uuid npm installed to create unique id for each note so it can be saved and clicked upon to open and deleted, html routes fixed and working, api routes added to get note, post note, and delete note

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 // require dependencies 
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const {v4: uuidv4} = require("uuid");
 
 // tell app to use port and default to 3001 if not set 
 const PORT = process.env.PORT || 3001;
@@ -13,16 +18,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// html routes created
-app.get("/html", (req, res) => {
+// html route created for landing page
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/html", (req, res) => {
-    res.sendFile(__dirname + "/notes.html");
+// html route created for notes page that user interacts with to write, save, and delete notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
-// API routes creaed 
+// getNotes() function created to get the notes created and parse them
+function getNotes() {
+return readFile("db/db.json", "utf-8").then(rawNotes => {
+    let parseNotes = [].concat(JSON.parse(rawNotes));
+    return parseNotes;
+})
+}
+
+// API route created to get the notes 
+app.get("/api/notes", (req, res) => {
+getNotes().then((parseNotes) => res.json(parseNotes));
+})
+
+// API route created to post the notes & save on the left side of the page
+// When click on each note on the side, it then shows up again for user
+app.post("/api/notes", (req, res) => {
+    getNotes().then(oldNotes => {
+    var noteObject = {title: req.body.title, text: req.body.text, id: uuidv4()};
+    var newArray = [...oldNotes, noteObject];
+    writeFile("db/db.json", JSON.stringify(newArray));
+    }).then(()=> res.json({msg: "ok"}));
+})
+
+// API route created to delete the previously saved note when the user clicks the trash can icon next to the note 
+app.delete("/api/notes/:id", (req, res) => {
+    getNotes()
+      .then((oldNotes) => {
+        var updatedArray = oldNotes.filter((note) => note.id !== req.params.id);
+        writeFile("db/db.json", JSON.stringify(updatedArray));
+      })
+      .then(() => res.json({ msg: "ok" }));
+})
+
 
 // set up listen method to make the server listen
 app.listen(PORT, () => {
